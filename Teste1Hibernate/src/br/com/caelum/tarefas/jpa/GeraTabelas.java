ecc1204949556567c4e4cb4dad682a757a1df13c
Francisco do Nascimento <chico.nascimento@gmail.com>
2018-02-10 01:02:13 -0300

    Update GeraTabelas.java

--- Teste1Hibernate/src/br/com/caelum/tarefas/jpa/GeraTabelas.java
+++ Teste1Hibernate/src/br/com/caelum/tarefas/jpa/GeraTabelas.java
@@ -7,9 +7,13 @@ public class GeraTabelas {
 	
 	public static void main(String[] args) {
 		System.out.println("ok");
+		try{
         EntityManagerFactory factory = Persistence.
         		createEntityManagerFactory("tarefas");
         factory.close();
+		} catch (Exception e) {
+		   e.printStackTrace();
+		}
     }
 	
 }
